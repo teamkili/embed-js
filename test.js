890e7c217e58166bed61481f12c67b52382c3f15
Krishna Nandakumar <nt.kris@gmail.com>
2023-03-25 12:42:39 +0000

    changes

--- test.js
+++ test.js
@@ -1,2 +1,14 @@
-const iframe = document.createElement("iframe");
-iframe.setAttribute("src", `https://www.kili.so/popup/${scriptTag.id}`);
+// Create a button element
+const button = document.createElement("button");
+
+// Set the button text
+button.textContent = "Click me!";
+
+// Set the button styles
+button.style.position = "fixed";
+button.style.bottom = "20px";
+button.style.right = "20px";
+button.style.padding = "10px";
+
+// Add the button to the page
+document.body.appendChild(button);
